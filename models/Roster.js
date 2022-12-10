75e5152fdf63cf51cd7d01b4fc534e020eaaeca7
Tessa Elyse <tessa.elise.robinson@gmail.com>
2022-12-10 13:22:37 -0700

    First draft of models completed

---
+++ models/Roster.js
@@ -0,0 +1,41 @@
+// requirements, consistent among all models (besides index)
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//
+class Roster extends Model {}
+
+// Roster.init => id, class_id(FK), (HAS MANY) student_id(FK)
+Roster.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      subject_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'subject',
+            key: 'id',
+          },
+      },
+      student_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'student',
+            key: 'id',
+          },
+      }
+    },
+    {
+        // ????????????????????????????????
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'roster',
+    }
+  );
+
+module.exports = Teacher;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,25 @@
-// require models
+// require models (e.g. Student, Subject, Teacher)
+const Student = require('./Student');
+const Teacher = require('./Teacher');
+const Subject = require('./Subject');
+const Roster = require ('./Roster');
 
-//
+// Associations
+Subject.belongsTo(Teacher, {
+    foreignKey: 'teacher_id'
+});
+
+Teacher.belongsTo(Subject, {
+    foreignKey: 'subject_id'
+});
+
+Roster.belongsTo(Subject, {
+    foriegnKey: 'subject_id'
+});
+
+Roster.hasMany (Student, {
+    foriegnKey: 'student_id'
+});
+
+// Export module
+module.exports = {Student, Teacher, Subject, Roster}
