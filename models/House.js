276406abc9c9ce172a7582581bc8414955c09f3e
92castro <celin92castro@gmail.com>
2022-12-12 14:10:31 -0700

    new house model

---
+++ models/House.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+//
+class House extends Model {}
+
+House.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    house_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // ????????????????????????????????
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "house",
+  }
+);
+
+module.exports = House;
