30e64158edec45d2301519ef83a67643cdfd010d
MandieBot <amandabotinelly@gmail.com>
2022-12-12 16:29:40 -0700

    Modified navbar and student profile handlebars

--- models/Student.js
+++ models/Student.js
@@ -43,13 +43,13 @@ Student.init(
         len: [6],
       },
     },
-    house_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "house",
-        key: "id",
-      },
-    },
+    // house_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: "house",
+    //     key: "id",
+    //   },
+    // },
   },
   {
     hooks: {
