9d76e386c20fd20a710e878ff3a712e74319171b
Tessa Elyse <tessa.elise.robinson@gmail.com>
2022-12-13 05:26:11 -0700

    added script to login.handlebars so current students section works!

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,9 @@
+// for query selector, how does it know to grab the id from the login.handlebars... i.e. how does in know which file it's looking in?
+
+// on the Student.js model, do we need lines 67-71 commented back in?
+
+// add a logoutbutton the the nav bar
+
 const loginFormHandler = async (event) => {
     event.preventDefault();
 
@@ -13,23 +19,26 @@ const loginFormHandler = async (event) => {
     
         if (response.ok) {
           document.location.replace('/');
+          console.log('asldkfj')
         } else {
           alert('Failed to log in.');
+          console.log('Failed to log in')
         }
       }
 };
 
-const signupFormHandler = async (event) => {
+const createFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const firstname = document.querySelector('#first-name').value.trim();
+    const lastname = document.querySelector('#last.name').value.trim();
+    const email = document.querySelector('#email-create').value.trim();
+    const password = document.querySelector('#password-create').value.trim();
   
     if (username && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ firstname, lastname, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -40,3 +49,11 @@ const signupFormHandler = async (event) => {
       }
     }
   };
+
+  document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.enroll-form')
+  .addEventListener('submit', createFormHandler);
