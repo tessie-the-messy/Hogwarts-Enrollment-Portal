830d479a93d24af58fd675f8302bfe7c01c47450
Tessa Elyse <tessa.elise.robinson@gmail.com>
2022-12-14 10:24:55 -0700

    Added buttons and routes to get profile and schedule pages

--- public/js/login.js
+++ public/js/login.js
@@ -1,54 +1,54 @@
 // add a logoutbutton the the nav bar
 
 const loginFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-    if (email && password) {
-        const response = await fetch('/api/students/login', {
-          method: 'POST',
-          body: JSON.stringify({ email, password }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-    
-        if (response.ok) {
-          document.location.replace('/landingpage');
-        } else {
-          alert('Failed to log in.');
-          console.log('Failed to log in')
-        }
-      }
+  if (email && password) {
+    const response = await fetch("/api/students/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/landingpage");
+    } else {
+      alert("Failed to log in.");
+      console.log("Failed to log in");
+    }
+  }
 };
 
 const enrollFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const firstName = document.querySelector('#first-name').value.trim();
-    const lastName = document.querySelector('#last-name').value.trim();
-    const email = document.querySelector('#email-enroll').value.trim();
-    const password = document.querySelector('#password-enroll').value.trim();
-  
-    if (firstName && lastName && email && password) {
-      const response = await fetch('/api/students', {
-        method: 'POST',
-        body: JSON.stringify({ firstName, lastName, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to sign up.');
-      }
-    }
-  };
+  event.preventDefault();
 
-  document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  const firstName = document.querySelector("#first-name").value.trim();
+  const lastName = document.querySelector("#last-name").value.trim();
+  const email = document.querySelector("#email-enroll").value.trim();
+  const password = document.querySelector("#password-enroll").value.trim();
+
+  if (firstName && lastName && email && password) {
+    const response = await fetch("/api/students", {
+      method: "POST",
+      body: JSON.stringify({ firstName, lastName, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/landingpage");
+    } else {
+      alert("Failed to sign up.");
+    }
+  }
+};
 
 document
-  .querySelector('.enroll-form')
-  .addEventListener('submit', enrollFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".enroll-form")
+  .addEventListener("submit", enrollFormHandler);
